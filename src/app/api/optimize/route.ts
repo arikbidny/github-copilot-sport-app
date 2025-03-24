e9c883872321eed576a56d498cbd79efc4e45c6c
Arik Bidny <arikbidny@Ariks-MacBook-Pro-2.local>
2025-03-24 11:00:40 +0200

    fixing error page

--- src/app/api/optimize/route.ts
+++ src/app/api/optimize/route.ts
@@ -1,25 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-
 export async function GET(request: NextRequest) {
   const startTime = Date.now();
   const arr = Array.from({ length: 100000 }, () =>
     Math.floor(Math.random() * 100000)
   );
 
-  function quickSort(arr: number[]): number[] {
-    if (arr.length <= 1) {
-      return arr;
+  let n = arr.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 1; i < n; i++) {
+      if (arr[i - 1] > arr[i]) {
+        let temp = arr[i - 1];
+        arr[i - 1] = arr[i];
+        arr[i] = temp;
+        swapped = true;
+      }
     }
-
-    const pivot = arr[Math.floor(arr.length / 2)];
-    const left = arr.filter((x) => x < pivot);
-    const right = arr.filter((x) => x > pivot);
-    const middle = arr.filter((x) => x === pivot);
-
-    return [...quickSort(left), ...middle, ...quickSort(right)];
-  }
-
-  const sortedArr = quickSort(arr);
+    n--;
+  } while (swapped);
 
   const endTime = Date.now();
   const executionTime = (endTime - startTime) / 1000; // Time in seconds
